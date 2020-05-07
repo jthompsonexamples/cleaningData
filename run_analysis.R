5723dec92331d1400d0e6f5499e48f12b8d1361e
Justin Thompson <stabemindaface@gmail.com>
2020-05-07 13:42:38 -0400

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -24,6 +24,8 @@ colNames <- read_lines('./UCI HAR Dataset/features.txt')
 activityLabel <- read_delim('./UCI HAR Dataset/activity_labels.txt', delim=' ',
                             col_names=c('num','Activity'))
 trainActivityNumber <- read_table('./UCI HAR Dataset/train/y_train.txt', col_names = 'num')
+subjectTrain <- read_delim('./UCI HAR Dataset/train/subject_train.txt', delim=' ',
+                           col_names = 'Subject')
 trainActivity <- right_join(activityLabel, trainActivityNumber)
 
 # get the training data, had to trim ws to fix a parse error
@@ -42,14 +44,15 @@ names(trainDF) <- colNames
 # is activity length the same as train set?
 nrow(trainActivity)==nrow(trainDF)
 # put them together
-trainDF <- trainActivity %>% transmute(Activity) %>% bind_cols(trainDF)
+trainDF <- trainActivity %>% transmute(Activity) %>% bind_cols(trainDF, subjectTrain)
 
 ## Do the test set
 # get the activities
 testActivityNumber <- read_table('./UCI HAR Dataset/test/y_test.txt', col_names = 'num')
 
 testActivity <- right_join(activityLabel, testActivityNumber)
-
+subjectTest <- read_delim('./UCI HAR Dataset/test/subject_test.txt', delim=' ',
+                           col_names = 'Subject')
 # get the training data, had to trim ws to fix a parse error
 testDF <- read_delim('./UCI HAR Dataset/test/X_test.txt', delim = ' ',
                       col_names=FALSE, trim_ws=TRUE)
@@ -66,7 +69,7 @@ names(testDF) <- colNames
 # is activity length the same as train set?
 nrow(testActivityNumber)==nrow(testDF)
 # put them together
-testDF <- testActivity %>% transmute(Activity) %>% bind_cols(testDF)
+testDF <- testActivity %>% transmute(Activity) %>% bind_cols(testDF,subjectTest)
 
 ## put both datasets together
 
@@ -74,7 +77,7 @@ fullDF <- bind_rows(trainDF, testDF)
 
 # get the mean and sd only
 n <- names(fullDF)
-tt <- grepl('mean|std|Activity', names(fullDF))
+tt <- grepl('mean[()]|std|Activity|Subject', names(fullDF))
 n <- n[tt]
 # clear the numbers
 n <- gsub('[0-9]+ ', '', n)
@@ -88,4 +91,8 @@ n <- gsub('-', '', n)
 subDF <- fullDF[,tt]
 names(subDF) <- n
 
-dataTidy <- gather(subDF, key='Feature', value = 'normalizedMeasurement')
+dataTidy <- subDF %>% 
+  group_by(Subject, Activity) %>% 
+  summarize_all(~mean(.))
+
+write.table(dataTidy, file='dataTidy.txt', row.names = FALSE)
